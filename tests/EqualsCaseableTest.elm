d314d9135818ad0231ded9b2a6c0c919bb82ff59
lue-bird <lue.the.bird@gmail.com>
2023-07-20 22:28:23 +0200

    indentation test +

--- tests/EqualsCaseableTest.elm
+++ tests/EqualsCaseableTest.elm
@@ -44,7 +44,53 @@ a =
                                 , "You can replace this check with a `case of` where you use the value you're matching for as a pattern."
                                 , "This can aid structuring your code in a way where the compiler knows as much about the current branch as you. Read more in the readme: https://dark.elm.dmy.fr/packages/lue-bird/elm-review-equals-caseable/latest/"
                                 ]
-                            , under = "=="
+                            , under = "list == []"
                             }
+                            |> Review.Test.whenFixed
+                                """module A exposing (..)
+a =
+    case list of
+        [] ->
+            "empty"
+        
+        _ ->
+            "filled"
+"""
+                        ]
+        , test "should report if == [] and keep indentation" <|
+            \() ->
+                """module A exposing (..)
+a =
+    List.reverse
+        (if list == [] then
+            "empty"
+
+         else
+            "filled"
+        )
+"""
+                    |> Review.Test.run (forbid EqualsCaseable.Everywhere)
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "equivalent `case of` exists"
+                            , details =
+                                [ "You are checking for equality against a value that could be a pattern in an equivalent `case of`!"
+                                , "You can replace this check with a `case of` where you use the value you're matching for as a pattern."
+                                , "This can aid structuring your code in a way where the compiler knows as much about the current branch as you. Read more in the readme: https://dark.elm.dmy.fr/packages/lue-bird/elm-review-equals-caseable/latest/"
+                                ]
+                            , under = "list == []"
+                            }
+                            |> Review.Test.whenFixed
+                                """module A exposing (..)
+a =
+    List.reverse
+        (case list of
+             [] ->
+                 "empty"
+             
+             _ ->
+                 "filled"
+        )
+"""
                         ]
         ]
