2cf80bb1a5eda775d0699723d4097926141f4f80
lue-bird <lue.the.bird@gmail.com>
2023-07-21 09:13:27 +0200

    more tests

--- tests/EqualsCaseableTest.elm
+++ tests/EqualsCaseableTest.elm
@@ -25,6 +25,14 @@ a =
 """
                     |> Review.Test.run (forbid EqualsCaseable.Everywhere)
                     |> Review.Test.expectNoErrors
+        , test "should not report an error on non-if condition with `forbid InIf`" <|
+            \() ->
+                """module A exposing (..)
+a =
+    list == []
+"""
+                    |> Review.Test.run (forbid EqualsCaseable.InIf)
+                    |> Review.Test.expectNoErrors
         , test "should report if == []" <|
             \() ->
                 """module A exposing (..)
@@ -55,6 +63,70 @@ a =
         
         _ ->
             "filled"
+"""
+                        ]
+        , test "should report if /= []" <|
+            \() ->
+                """module A exposing (..)
+a =
+    if list /= [] then
+        "filled"
+
+    else
+        "empty"
+"""
+                    |> Review.Test.run (forbid EqualsCaseable.Everywhere)
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "equivalent `case of` exists"
+                            , details =
+                                [ "You are checking for equality against a value that could be a pattern in an equivalent `case of`!"
+                                , "You can replace this check with a `case of` where you use the value you're matching for as a pattern."
+                                , "This can aid structuring your code in a way where the compiler knows as much about the current branch as you. Read more in the readme: https://dark.elm.dmy.fr/packages/lue-bird/elm-review-equals-caseable/latest/"
+                                ]
+                            , under = "list /= []"
+                            }
+                            |> Review.Test.whenFixed
+                                """module A exposing (..)
+a =
+    case list of
+        [] ->
+            "empty"
+        
+        _ ->
+            "filled"
+"""
+                        ]
+        , test "should report if == [] && == []" <|
+            \() ->
+                """module A exposing (..)
+a =
+    if aList == [] && bList == [] then
+        "empty"
+
+    else
+        "a and or b is filled"
+"""
+                    |> Review.Test.run (forbid EqualsCaseable.Everywhere)
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "equivalent `case of` exists"
+                            , details =
+                                [ "You are checking for equality against a value that could be a pattern in an equivalent `case of`!"
+                                , "You can replace this check with a `case of` where you use the value you're matching for as a pattern."
+                                , "This can aid structuring your code in a way where the compiler knows as much about the current branch as you. Read more in the readme: https://dark.elm.dmy.fr/packages/lue-bird/elm-review-equals-caseable/latest/"
+                                ]
+                            , under = "aList == [] && bList == []"
+                            }
+                            |> Review.Test.whenFixed
+                                """module A exposing (..)
+a =
+    case ( aList, bList ) of
+        ( [], [] ) ->
+            "empty"
+        
+        _ ->
+            "a and or b is filled"
 """
                         ]
         , test "should report if == [] and keep indentation" <|
@@ -92,5 +164,39 @@ a =
                  "filled"
         )
 """
+                        ]
+        , test "should report (/=) []" <|
+            \() ->
+                """module A exposing (..)
+a =
+    [ (/=) [] ]
+"""
+                    |> Review.Test.run (forbid EqualsCaseable.Everywhere)
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "equivalent `case of` exists"
+                            , details =
+                                [ "You are checking for equality against a value that could be a pattern in an equivalent `case of`!"
+                                , "You can replace this check with a `case of` where you use the value you're matching for as a pattern."
+                                , "This can aid structuring your code in a way where the compiler knows as much about the current branch as you. Read more in the readme: https://dark.elm.dmy.fr/packages/lue-bird/elm-review-equals-caseable/latest/"
+                                , """Note: Since your condition is a curried prefix operation, I don't have a variable name I can use to fix it automatically. An example:
+
+List.all ((/=) [])
+
+as a case of:
+
+List.all
+    (\\list ->
+        case list of
+            [] ->
+                False
+            
+            _ :: _ ->
+                True
+    )
+"""
+                                ]
+                            , under = "(/=) []"
+                            }
                         ]
         ]
